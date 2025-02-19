a4b7e396965edf46abb324eabff0b88b1128af33
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-02-20 02:26:04 +0600

    error page done

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-200 flex-col text-center gap-4">
+      <h1 className="text-6xl font-black">404</h1>
+      <h1 className="text-5xl font-black text-purple-950">Page Not Found</h1>
+      <p>We couldn&apos;t find the page you were looking for.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/root/router.jsx
+++ src/root/router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
 import Login from "../Pages/Login/Login";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      errorElement:<ErrorPage></ErrorPage>,
       children:[
         {
             path:"/",
