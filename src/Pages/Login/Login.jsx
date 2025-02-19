b25fbf3edfc432bf727213bf93c507b7df94701e
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-02-20 02:14:15 +0600

    login page design modified

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { Fade } from "react-awesome-reveal";
 import donezoLogo from "../../assets/denzoLogo/denzo-bg-remove.png";
 import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
@@ -8,18 +9,18 @@ const Login = () => {
       {/* Left Section - Login Form */}
       <div className="flex flex-col items-center justify-center flex-1 px-6 md:px-12 mt-10">
         <Fade direction="left">
-          <div className="bg-violet-800 shadow-2xl p-8 rounded-xl w-full max-w-md">
-            <h1 className="text-3xl font-bold text-white text-center">
+          <div className="bg-purple-200 shadow-2xl p-8 rounded-xl w-full max-w-md ">
+            <h1 className="text-3xl font-bold text-purple-950 text-center ">
               Welcome Back!
             </h1>
-            <p className="text-gray-200 text-center mt-2">
+            <p className="text-gray-600 text-center mt-2">
               Login to continue managing your tasks.
             </p>
 
             <div className="mt-6 space-y-4">
               {/* Email Input */}
               <div>
-                <label className="block text-gray-300 text-sm font-medium mb-1">
+                <label className="block text-black text-sm font-medium mb-1">
                   Email
                 </label>
                 <input
@@ -31,7 +32,7 @@ const Login = () => {
 
               {/* Password Input */}
               <div>
-                <label className="block text-gray-300 text-sm font-medium mb-1">
+                <label className="block text-black text-sm font-medium mb-1">
                   Password
                 </label>
                 <input
@@ -43,7 +44,7 @@ const Login = () => {
 
               {/* Login Button */}
               <button
-                className="w-full bg-purple-500 hover:bg-purple-700 text-white py-3 rounded-lg 
+                className="w-full bg-purple-800 hover:bg-purple-950 text-white py-3 rounded-lg 
                 font-semibold transition-all duration-300 cursor-pointer"
               >
                 Login
@@ -53,20 +54,21 @@ const Login = () => {
             {/* Divider */}
             <div className="flex items-center justify-center my-4">
               <span className="w-1/3 border-t border-gray-400"></span>
-              <span className="mx-3 text-gray-300">OR</span>
+              <span className="mx-3 text-black">OR</span>
               <span className="w-1/3 border-t border-gray-400"></span>
             </div>
 
             {/* Google Login Button */}
-            <button className="w-full flex items-center justify-center gap-2 border border-gray-400 text-gray-100 py-3 rounded-lg font-semibold hover:bg-gray-700 transition-all duration-200">
+            <button className="w-full flex items-center justify-center gap-2 text-white py-3 rounded-lg font-semibold bg-purple-800 hover:bg-purple-950 transition-all duration-200 cursor-pointer">
+            <FcGoogle className="text-xl"/>
               Login with Google
             </button>
 
-            <p className="mt-4 text-gray-200">
+            <p className="mt-4 text-black">
               Don&apos;t have an account?{" "}
               <Link
                 to={"/register"}
-                className="font-bold text-purple-300 hover:text-purple-400 underline"
+                className="font-bold text-purple-800 hover:text-purple-950 underline"
               >
                 Register Now
               </Link>
